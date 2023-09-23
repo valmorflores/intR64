08e5c37c852cbef80a8ce4c62bd8c2f19a2f1c8c
valmorflores <valmorflores@gmail.com>
2023-09-23 14:39:45 -0300

    update official path file /var/rinha/source.rinha.json

--- src/main.dart
+++ src/main.dart
@@ -2,7 +2,8 @@ import 'interpreter.dart';
 
 main() {
   //String filePath = "c:\\dev\\dart\\intR64\\bin\\fib.json";
-  String filePath = "c:\\dev\\dart\\intR64\\bin\\print.json";
+  //String filePath = "c:\\dev\\dart\\intR64\\bin\\print.json";
+  String filePath = "/var/rinha/source.rinha.json";
   Interpreter interpreter = Interpreter(file: filePath);
   interpreter.run();
 }
